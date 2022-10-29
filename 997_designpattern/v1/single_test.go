bf2707f0e529f30a2eac3b0b63c45cddc5c51da0
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2022-10-29 14:50:39 +0900

    docs golang singleton

---
+++ 997_designpattern/v1/single_test.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
+
+var lock = &sync.Mutex{}
+
+type single struct {
+}
+
+var singleInstance *single
+
+func getInstance(wg *sync.WaitGroup) *single {
+	if singleInstance == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if singleInstance == nil {
+			fmt.Println("Normal : Object have been created")
+			singleInstance = &single{}
+		} else {
+			fmt.Println("Normal : Object have been already created")
+		}
+	} else {
+		fmt.Println("Normal : Object have been already created")
+	}
+	wg.Done()
+	return singleInstance
+}
+
+var once sync.Once
+
+func getInstanceWithOnce(wg *sync.WaitGroup) *single {
+	if singleInstance == nil {
+		once.Do(
+			func() {
+				fmt.Println("Once : Creating single instance now")
+				singleInstance = &single{}
+			})
+	} else {
+		fmt.Println("Once : Single instance already created.")
+	}
+	wg.Done()
+	return singleInstance
+}
+
+func TestSingleton(t *testing.T) {
+
+	ws := sync.WaitGroup{}
+
+	for i := 0; i < 30; i++ {
+		ws.Add(1)
+		go getInstance(&ws)
+	}
+
+	for i := 0; i < 30; i++ {
+		ws.Add(1)
+		go getInstanceWithOnce(&ws)
+	}
+
+	ws.Wait()
+}
