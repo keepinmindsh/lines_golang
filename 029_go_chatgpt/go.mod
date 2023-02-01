bbcf1e6d5a20e447205925c7cd3481b7137ccda7
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-02-01 13:12:02 +0900

    chat-gpt golang sample

---
+++ 029_go_chatgpt/go.mod
@@ -0,0 +1,5 @@
+module 029_go_chatgpt
+
+go 1.19
+
+require github.com/sashabaranov/go-gpt3 v0.0.0-20230128191859-3695eb3ade92 // indirect
---
+++ 029_go_chatgpt/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	gogpt "github.com/sashabaranov/go-gpt3"
+)
+
+func main() {
+	c := gogpt.NewClient("")
+	ctx := context.Background()
+
+	req := gogpt.CompletionRequest{
+		Model:     gogpt.GPT3Ada,
+		MaxTokens: 5,
+		Prompt:    "hey!",
+	}
+	resp, err := c.CreateCompletion(ctx, req)
+	if err != nil {
+		return
+	}
+	fmt.Println(resp.Choices[0].Text)
+}
