558705199ea02b4eaff68cea601540a758edfd08
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-05-10 21:16:58 +0900

    rxgo - interval sample

---
+++ 030_go_rx/tests/rx_call_interval_test.go
@@ -0,0 +1,22 @@
+package tests
+
+import (
+	"fmt"
+	"github.com/reactivex/rxgo/v2"
+	"testing"
+	"time"
+)
+
+func Test_IntervalCallSample(t *testing.T) {
+	observable := rxgo.Interval(rxgo.WithDuration(5 * time.Second))
+
+	observable.DoOnNext(func(i interface{}) {
+		fmt.Printf("%v", i)
+	})
+
+	observe := observable.Observe()
+
+	item := <-observe
+
+	fmt.Printf("%v", item.V)
+}
