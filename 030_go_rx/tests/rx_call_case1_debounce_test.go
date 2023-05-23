3b5b984a64f0391319601c8bb4e8729f96a8970a
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-05-24 01:09:30 +0900

    debounce sample

--- 030_go_rx/tests/rx_call_case1_debounce_test.go
+++ 030_go_rx/tests/rx_call_case1_debounce_test.go
@@ -2,16 +2,24 @@ package tests
 
 import (
 	"github.com/reactivex/rxgo/v2"
+	"sync"
 	"testing"
 	"time"
 )
 
+// FIXME 활용 부분에 대해서 검토 예정
 func Test_Debounce(t *testing.T) {
 	observable := rxgo.Just(1, 2, 3, 4, 5)()
 
+	var wg sync.WaitGroup
+
+	wg.Add(1)
 	debounce := observable.Debounce(rxgo.WithDuration(250 * time.Millisecond))
 
 	for item := range debounce.Observe() {
 		t.Log(item.V)
+		wg.Done()
 	}
+
+	wg.Wait()
 }
