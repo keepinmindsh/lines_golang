8919931d441a0a3c42e8887637bac98bd74d52ef
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-06-24 20:27:39 +0900

    pattern addicted - hell code

--- 997_designpattern/v1/design_pattern_hell_code.go
+++ 997_designpattern/v1/design_pattern_hell_code.go
@@ -1,7 +1,48 @@
 package designpattern
 
-func NormalCode() {
-	printHello := "Hello"
+import "fmt"
 
-	print(printHello)
+func PatternAddicted() {
+	hello := NewFactoryHello(PRINT)
+
+	err := hello.Do("hello")
+	if err != nil {
+		panic(err)
+	}
+}
+
+type (
+	action interface {
+		Do(value any) error
+	}
+)
+type Print struct{}
+type ACTION string
+
+const (
+	PRINT ACTION = "Print"
+)
+
+func NewFactoryHello(action ACTION) action {
+	switch action {
+	case PRINT:
+		return NewPrintAction()
+	default:
+		return nil
+	}
+}
+
+func NewPrintAction() action {
+	return Print{}
+}
+
+func (p Print) Do(value any) error {
+	s, ok := value.(string)
+	if ok {
+		fmt.Println(s)
+		return nil
+	} else {
+		err := fmt.Errorf("error type is wrong")
+		return err
+	}
 }
