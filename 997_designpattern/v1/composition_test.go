702b9a155dddb9518f645310c68dc4b7f92b5630
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-06-24 23:44:37 +0900

    composition test for sample study code

---
+++ 997_designpattern/v1/composition_test.go
@@ -0,0 +1,24 @@
+package designpattern
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_CompositionSample(t *testing.T) {
+	Process(&Object1{})
+}
+
+type Composition interface {
+	Related()
+}
+
+func Process(composition Composition) {
+	composition.Related()
+}
+
+type Object1 struct{}
+
+func (o *Object1) Related() {
+	fmt.Println("This is composition sample.")
+}
