2dc5c1fc3346431cc2b868a202e30de9fafb3d0d
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-07-14 23:49:06 +0900

    interface - steering

--- 997_designpattern/v1/pattern/srp/car/car.go
+++ 997_designpattern/v1/pattern/srp/car/car.go
@@ -4,8 +4,9 @@ import "designpattern_v1/v1/pattern/srp/domain"
 
 type Car struct {
 	domain.Tire
+	domain.Steering
 }
 
-func NewCar(tire domain.Tire) Car {
-	return Car{tire}
+func NewCar(tire domain.Tire, steering domain.Steering) Car {
+	return Car{tire, steering}
 }
