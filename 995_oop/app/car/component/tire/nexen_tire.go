d94eaa2be33c826bd0cc9aa15a0dc936f64a2713
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2023-08-06 22:17:09 +0900

    fmt.Printf carrage return added

--- 995_oop/app/car/component/tire/nexen_tire.go
+++ 995_oop/app/car/component/tire/nexen_tire.go
@@ -36,11 +36,11 @@ func (v *nexenTire) MoveForward() {
 			fmt.Println("your car cannot move forward anymore!")
 		}
 
-		fmt.Printf("Straight, Current Position - x :%v, y :%v", v.maps.CurrentX, v.maps.CurrentY)
+		fmt.Printf("Straight, Current Position - x : %v, y : %v \r\n", v.maps.CurrentX, v.maps.CurrentY)
 	case code.RightDiagonal:
-		fmt.Printf("After RightDiagonal, Current Position - x :%v, y :%v", v.maps.CurrentX, v.maps.CurrentY)
+		fmt.Printf("After RightDiagonal, Current Position - x : %v, y : %v \r\n", v.maps.CurrentX, v.maps.CurrentY)
 	case code.LeftDiagonal:
-		fmt.Printf("After LeftDiagonal, Current Position - x :%v, y :%v", v.maps.CurrentX, v.maps.CurrentY)
+		fmt.Printf("After LeftDiagonal, Current Position - x : %v, y : %v \r\n", v.maps.CurrentX, v.maps.CurrentY)
 	}
 }
 
@@ -59,11 +59,11 @@ func (v *nexenTire) MoveBackward() {
 			v.maps.CurrentX = v.maps.CurrentX - 1
 		}
 
-		fmt.Printf("Straight, Current Position - x :%v, y : %v", v.maps.CurrentX, v.maps.CurrentY)
+		fmt.Printf("Straight, Current Position - x : %v, y : %v \r\n", v.maps.CurrentX, v.maps.CurrentY)
 	case code.RightDiagonal:
-		fmt.Printf("After RightDiagonal, Current Position - x :%v, y : %v", v.maps.CurrentX, v.maps.CurrentY)
+		fmt.Printf("After RightDiagonal, Current Position - x : %v, y : %v \r\n", v.maps.CurrentX, v.maps.CurrentY)
 	case code.LeftDiagonal:
-		fmt.Printf("After LeftDiagonal, Current Position - x :%v, y : %v", v.maps.CurrentX, v.maps.CurrentY)
+		fmt.Printf("After LeftDiagonal, Current Position - x : %v, y : %v \r\n", v.maps.CurrentX, v.maps.CurrentY)
 	}
 
 }
